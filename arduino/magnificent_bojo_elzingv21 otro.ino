f19612f8eab7c8ebc3ee2d60153bfae2214d3728
NemiadLu <tomastorres1009008@gmail.com>
2026-04-14 10:28:11 -0300

    hd

---
+++ arduino/magnificent_bojo_elzingv21 otro.ino
@@ -0,0 +1,76 @@
+  #define cangrejo A0
+  #define TMP A5
+  #define LDR A3
+  #define LED_R 3
+  #define LED_G 6	
+  #define LED_B 5
+  #define BTN_U 7
+  #define BTN_D 8	
+  #define BTN 10
+  #define FTR 9
+  //#define MOSTRAR 
+  //#define MOSTRARTEMP
+  //#define MOSTRARLUZ
+  //al de arriba se hace para habilitar lugares.
+int POVIS;
+float temp;
+int luz;
+// Para las resistencias comunes, utiliza 220 Ohm, no KOhm
+void setup()
+{
+  pinMode(cangrejo, INPUT);
+  pinMode(TMP, INPUT);
+  pinMode(LDR, INPUT);
+  pinMode(LED_R, OUTPUT);
+  pinMode(LED_G, OUTPUT);
+  pinMode(LED_B, OUTPUT); 
+  pinMode(BTN_U, INPUT);
+  pinMode(BTN_D, INPUT);
+  pinMode(BTN, INPUT_PULLUP);
+  pinMode(FTR, INPUT);
+  Serial.begin(9600);
+}
+void loop() {
+  //int/bool push = digitalRead(Variable); ambos son validos
+  int push = digitalRead(BTN); 
+  //Serial.println(push);
+  int push2 = digitalRead(BTN_U); 
+  //Serial.println(push2);
+  int push3 = digitalRead(BTN_D); 
+  //Serial.println(push3);
+   /*if (push == LOW){
+     digitalWrite(LED_R, 1);
+     }else{
+     digitalWrite(LED_R, 0);
+  }*//*
+  bool LUZ2 = digitalRead(FTR);
+  Serial.print(LUZ2);*/
+  POVIS = analogRead(cangrejo);
+
+  #ifdef MOSTRAR
+    Serial.print("La resistencia es de:");
+    Serial.println(POVIS);
+  #endif
+  // 0-255 Escritura
+  // 0-1023 Lectura
+  temp = (analogRead(TMP) * 5.0 / 1024) * 100 - 50;
+  
+  #ifdef MOSTRARTEMP
+  Serial.print("Temp:");
+  Serial.println(temp);
+  #endif
+  luz = (analogRead(LDR));
+  //valores; map(Value, MinOG, MaxOG, MinNew, MaxNew);
+  //luz = map(luz, 1, 310, 100, 0);
+  int brillo = map(luz, 0, 1023, 255, 0); 
+  
+  analogWrite(LED_R, brillo);
+  analogWrite(LED_G, brillo);
+  analogWrite(LED_B, brillo);
+
+  #ifdef MOSTRARLUZ
+  Serial.print("Luz:");
+  Serial.println(luz);
+  //delay(1000);
+  #endif
+}
